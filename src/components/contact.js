c2d742ff48d737d4115160a73293c3f773ba6c48
Camerone Stoney <Cameronestoney@yahoo.com>
2019-08-19 10:04:53 -0700

    contact info added. styling still needed

--- src/components/contact.js
+++ src/components/contact.js
@@ -2,10 +2,26 @@ import React from "react";
 
 const Contact = () => {
     return(
-        <div className="container">
-            <h4 className="...">Contact</h4>
-            {/* content in here */}
+        <div id="contact-content">
+        <h1 className="section_header" id="contact">Contact</h1>
+        <div className="row">
+            <div className="col-xl-4">
+                <a href="https://www.linkedin.com/in/camerone-stoney-b8a798177/">
+                    <img id="linkedin_icon" className="contact-icon" src="assets/images/LI-In-Bug.png" />
+                </a>
+            </div>
+            <div className="col-xl-4">
+                <a href="https://github.com/Cjstoney">
+                    <img id="github_icon" className="contact-icon" src="assets/images/GitHub-Mark-64px.png" />
+                </a>
+            </div>
+            <div className="col-xl-4">
+                <a href="assets/pdfs/document.pdf">
+                    <img id="pdf_icon" className="contact-icon" src="assets/images/iconfinder_file-pdf_383140.png" />
+                </a>
+            </div>
         </div>
+    </div>
     )
 }
 
