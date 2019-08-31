a4ff5a8f35d99c5ac14cc3e28196b08564854c84
Camerone Stoney <Cameronestoney@yahoo.com>
2019-08-30 21:26:41 -0700

    navbar finally has a row and the grid system in place for the links

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,38 +4,23 @@ import "./navbar.css";
 
 function Navbar(){
   return (
-    <nav>
+    <nav >
 
-      <div className=" navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav">
-          <li className="nav-item">
-            <Link
-              to="/" 
-              className={
-                window.location.pathname === "/" || window.location.pathname === "/"
-                  ? "nav-link active"
-                  : "nav-link"
-              } >Home </Link>
-          </li>
-          <li className="nav-item">
-            <Link 
-            to="/about"  className={
-              window.location.pathname === "/" || window.location.pathname === "/"
-                ? "nav-link active"
-                : "nav-link"
-            } >About</Link>
-          </li> 
-          <li className="nav-item">
-            <Link
-            to="/portfolio" className="nav-link" >Portfolio</Link>
-          </li>
-          <li className="nav-item">
+        <ul className="navbar-nav grid-container">
+            <Link to="/" className="nav-link">Home </Link>
+          
+          
+            <Link  to="/about"  className="nav-link">About</Link>
+           
+          
+            <Link to="/portfolio" className="nav-link" >Portfolio</Link>
+          
+          
             <Link className="nav-link" to="/contact">Contact</Link>
-          </li>
+          
 
         </ul>
 
-      </div>
     </nav>
   )
 }
