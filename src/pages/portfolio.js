dbbd5b0096ecb0da049a7bc6182cb6669a5a5e7a
Camerone Stoney <Cameronestoney@yahoo.com>
2019-09-03 19:45:37 -0700

    portfolio grid complete and working

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PortfolioCard from "../components/portfolioCard";
 import cards from "../cards.json"
+import "./portfolio.css"
 
 const Portfolio = () => {
     return (
@@ -8,23 +9,24 @@ const Portfolio = () => {
             <div className="row">
                 <div id="portfolio-content">
                     <h1 className="section_header" id="portfolio">Portfolio</h1>
+                </div >
+                <div className="portfolio-map">
+                    {
+                        cards.map(data => (
+                            <PortfolioCard
+                                id={data.id}
+                                key={data.id}
+                                name={data.projName}
+                                image={data.image}
+                                url={data.URL}
+                            />
+                        ))
+                    }
                 </div>
-                {
-                cards.map(data => (
-                    <PortfolioCard
-                    id={data.id}
-                    key={data.id}
-                    name={data.projName}
-                    image={data.image}
-                    url={data.URL}
-                />
-                ))
-                }
-        
             </div>
         </div>
-    
-    
+
+
     )
 }
 
