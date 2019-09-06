fc35291366196ae4a422d47a1c4d0c856e879dba
Camerone Stoney <Cameronestoney@yahoo.com>
2019-09-05 18:13:24 -0700

    background image changed, text decoration removed from titles, and positioning figured out

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,11 +3,10 @@ import './home.css';
 
 const Home = () => {
     return(
-        <div id="leftSideCol">
                 <div id="landing-section">
                     <h1 id="name">Camerone Stoney</h1>
                 </div>
-            </div>
+            
     )
 }
 
