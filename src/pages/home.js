424aa52fbdb8c62e0c06c279d8e5a65ceda33f5c
Camerone Stoney <Cameronestoney@yahoo.com>
2019-09-10 12:30:22 -0700

    home page animation and css adjusted

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,11 +2,20 @@ import React from "react";
 import './home.css';
 
 const Home = () => {
-    return(
-                <div id="landing-section">
-                    <h1 id="name">Camerone Stoney</h1>
-                </div>
-            
+    return (
+        <div id="landing-section">
+            <div className="text-container">
+            <p className="static-text">Hi, I'm</p>
+            <ul className="dynamic-text">
+                <li className="item">Camerone Stoney </li>
+                <li className="item">a web developer</li>
+                {/* <li className="item">UX/UI desiger</li>
+                <li className="item">content creator</li>
+                <li className="item">professor</li> */}
+            </ul>
+        </div>
+        </div>
+
     )
 }
 
