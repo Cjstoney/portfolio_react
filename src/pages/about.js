235e324aa26335c451bf20e473d84aca7c48dd01
Camerone Stoney <Cameronestoney@yahoo.com>
2019-10-04 12:08:26 -0700

    work continues

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
 import "./about.css";
 
 const About = () => {
@@ -10,29 +12,29 @@ const About = () => {
 
             <div className='about-content'>
 
-                <div >
-                    <img className='about-img' id="cs-image" src="../images/IMG_1483 2.jpg" alt=" Camerone Stoney" />
+                <img className='about-img bio' id="cs-image" src="../images/IMG_1483 2.jpg" alt=" Camerone Stoney" />
 
-                    <h2 className='bio' id="cs-text">I am Camerone Stoney. I moved from the San Francisco Bay Area to Portland,
+                <h2 className='bio bio-text' id="cs-text">I am Camerone Stoney. I moved from the San Francisco Bay Area to Portland,
                         Oregon in 2015.</h2>
 
 
-                    <img className='about-img' id="sf-image" src="../images/IMG_1483 2.jpg" alt=" Camerone Stoney" />
-
-                    <h2 className='bio' id="sf-text"> While in the
-                     Bay Area I studied Fine Art with an emphasis in painting at The San Francisco Art
+                <h2 className='bio bio-text' id="sf-text"> While in the
+                 Bay Area I studied Fine Art with an emphasis in painting at The San Francisco Art
                         Institute.</h2>
+                <img className='about-img bio' id="sf-image" src="../images/bay_area.png" alt=" Camerone Stoney" />
 
-                    <img className='about-img' id="dev-image" src="../images/IMG_1483 2.jpg" alt=" Camerone Stoney" />
+                <img className='about-img bio' id="dev-image" src="../images/portland.png" alt=" Camerone Stoney" />
 
-                    <h2 className='bio' id="dev-text">After
-                    moving to Portland, I worked for three years as a fine woodworker. I have since begun
-                    working in web
-                    development, using my art background to influence my aesthetic choices and my
-                    woodworking production
+                <h2 className='bio bio-text' id="dev-text">After
+                moving to Portland, I worked for three years as a fine woodworker. I have since begun
+                working in web
+                development, using my art background to influence my aesthetic choices and my
+                woodworking production
                     knowledge to build websites from start to finish. </h2>
-                </div>
             </div>
+
+            <Link to='/Portfolio' className='route' >Projects</Link>
+
         </div>
 
     )
