e41e4eefff5d31706e35c6a2cc5b51587be18aca
Camerone Stoney <Cameronestoney@yahoo.com>
2019-10-04 14:47:43 -0700

    unicode arrow added to the link for projects

--- src/pages/about.js
+++ src/pages/about.js
@@ -34,7 +34,7 @@ const About = () => {
             </div>
             <hr className='break'></hr>
             <div className='forLink'>
-                <Link to='/Portfolio' className='route' >Projects</Link>
+                <Link to='/Portfolio' className='route' >Projects &#8594;</Link>
             </div>
         </div>
 
