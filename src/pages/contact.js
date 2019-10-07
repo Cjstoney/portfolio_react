13e08b420d97e87d039d3582e2404606fa75e2ae
Camerone Stoney <Cameronestoney@yahoo.com>
2019-10-07 14:10:17 -0700

    contact page styling updated "

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -5,22 +5,25 @@ const Contact = () => {
     return(
         <div className="contact-page">
         <h1 className="section_header" id="contact">Contact</h1>
+        <div className="grid">
         <div  className="contact-container">
             <div className="icons">
-                <a href="https://www.linkedin.com/in/camerone-stoney-b8a798177/">
+                {/* <a href="https://www.linkedin.com/in/camerone-stoney-b8a798177/"> */}
                     <img id="linkedin_icon" className="contact-icon" src="../images/LI-In-Bug.png" alt="Linkedin Icon" />
-                </a>
+                {/* </a> */}
+            <a  href="https://www.linkedin.com/in/camerone-stoney-b8a798177/"><div className='contactBtn' >Check out my LinkedIn here</div></a>
             </div>
             <div className="icons">
-                <a href="https://github.com/Cjstoney">
                     <img id="github_icon" className="contact-icon" src="../images/GitHub-Mark-64px.png" alt="Github Icon" />
+                <a  href="https://github.com/Cjstoney"> <div className='contactBtn'>See some of my code here</div>
                 </a>
             </div>
             <div className="icons">
-                <a href="assets/pdfs/document.pdf">
                     <img id="pdf_icon" className="contact-icon" src="../images/iconfinder_file-pdf_383140.png" alt="PDF portfolio Icon" />
+                <a  href="assets/pdfs/document.pdf"><div className='contactBtn'>Take a look at my resume here</div>
                 </a>
             </div>
+        </div>
         </div>
     </div>
     )
