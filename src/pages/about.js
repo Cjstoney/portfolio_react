94b5deb7b74b712d261ac2d639c3d0808e9240cd
Camerone Stoney <Cameronestoney@yahoo.com>
2019-10-07 13:27:15 -0700

    Got rid of the home page images

--- src/pages/about.js
+++ src/pages/about.js
@@ -12,25 +12,13 @@ const About = () => {
 
             <div className='about-content'>
 
-                <img className='about-img bio' id="cs-image" src="../images/IMG_1483 2.jpg" alt=" Camerone Stoney" />
+                {/* <img className='about-img bio' id="cs-image" src="../images/IMG_1483 2.jpg" alt=" Camerone Stoney" /> */}
 
                 <h2 className='bio bio-text' id="cs-text">I am Camerone Stoney. I moved from the San Francisco Bay Area to Portland,
-                        Oregon in 2015.</h2>
-
-
-                <h2 className='bio bio-text' id="sf-text"> While in the
-                 Bay Area I studied Fine Art with an emphasis in painting at The San Francisco Art
-                        Institute.</h2>
-                <img className='about-img bio' id="sf-image" src="../images/bay_area.png" alt=" Camerone Stoney" />
-
-                <img className='about-img bio' id="dev-image" src="../images/portland.png" alt=" Camerone Stoney" />
-
-                <h2 className='bio bio-text' id="dev-text">After
-                moving to Portland, I worked for three years as a fine woodworker. I have since begun
-                working in web
-                development, using my art background to influence my aesthetic choices and my
-                woodworking production
-                    knowledge to build websites from start to finish. </h2>
+                Oregon in 2015. While in the Bay Area I studied Fine Art with an emphasis in painting at The San Francisco Art
+                Institute.After moving to Portland, I worked for three years as a fine woodworker. I have since begun
+                working in web development, using my art background to influence my aesthetic choices and my
+                woodworking production knowledge to build websites from start to finish. </h2>
             </div>
             <hr className='break'></hr>
             <div className='forLink'>
