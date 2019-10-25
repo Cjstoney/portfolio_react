9a3065a82a2de083d6b1ec7b57681b26ed93fb72
Camerone Stoney <Cameronestoney@yahoo.com>
2019-10-25 08:59:24 -0700

    fixed typo on about section

--- src/pages/about.js
+++ src/pages/about.js
@@ -16,8 +16,8 @@ const About = () => {
                 {/* <img className='about-img bio' id="cs-image" src="../images/IMG_1483 2.jpg" alt=" Camerone Stoney" /> */}
 
                 <p className='bio bio-text' id="cs-text">I am Camerone Stoney. I moved from the San Francisco Bay Area to Portland,
-                Oregon in 2015. While in the Bay Area I studied Fine Art with an emphasis in painting at The San Francisco Art
-                Institute.After moving to Portland, I worked for three years as a fine woodworker. I have since begun
+                Oregon in 2015. While in the Bay Area, I studied Fine Art with an emphasis in painting at The San Francisco Art
+                Institute. After moving to Portland, I worked for three years as a fine woodworker. I have since begun
                 working in web development, using my art background to influence my aesthetic choices and my
                 woodworking production knowledge to build websites from start to finish. </p>
             </div>
