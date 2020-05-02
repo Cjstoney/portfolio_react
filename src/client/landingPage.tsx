c62e62258cc6ce93093fffa453cb2e7f56812a5f
Camerone Stoney <Cameronestoney@yahoo.com>
2020-05-01 19:33:40 -0700

    style(client):landing page styles started.

--- src/client/landingPage.tsx
+++ src/client/landingPage.tsx
@@ -1,12 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./landingPage.css";
 
 class LandingPage extends React.Component {
   // state = {};
   render() {
     return (
-      <div>
-        <Link to="/webHome">web</Link>
+      <div className="landing-page-containter">
+        <Link id="webHome" className="landingPage-link" to="/webHome">
+          Web
+        </Link>
+        <Link id="woodHome" className="landingPage-link" to="/woodHome">
+          Wood
+        </Link>
+        <Link id="artHome" className="landingPage-link" to="/artHome">
+          Art
+        </Link>
       </div>
     );
   }
