a7f9456620365fa26075bca66a35985c94ebc132
Camerone Stoney <Cameronestoney@yahoo.com>
2020-05-01 19:31:32 -0700

    fix(client): Routes now work

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import WebHome from "./client/portfolioTypes/web/webHome";
 import LandingPage from "./client/landingPage";
 import Portfolio from "./client/portfolioTypes/web/pages/portfolio";
 import Contact from "./client/portfolioTypes/web/pages/contact";
 import { RouteDefs } from "./Routes";
-// import {RouteDefs } from './Routes'
-
-const { WEB_CONTACT } = RouteDefs;
 
 class App extends React.Component {
   state = {};
@@ -15,10 +12,12 @@ class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
-        <Route exactPath="/" component={LandingPage} />
-        <Route path="/webHome" component={WebHome} />
-        <Route path="/webPortfolio" component={Portfolio} />
-        <Route path="/webContact" component={Contact} />
+        <Switch>
+          <Route exact path="/" component={LandingPage} />
+          <Route path="/webHome" component={WebHome} />
+          <Route path="/webPortfolio" component={Portfolio} />
+          <Route path="/webContact" component={Contact} />
+        </Switch>
       </BrowserRouter>
     );
   }
