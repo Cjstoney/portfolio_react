ea91da17b157f96137459d41c212eb9987db1631
Camerone Stoney <Cameronestoney@yahoo.com>
2020-12-01 15:00:08 -0800

    refactor: small navbar style changes

--- src/components/parts/navbar/navbar.js
+++ src/components/parts/navbar/navbar.js
@@ -1,23 +1,25 @@
 import React from "react";
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 import "./navbar.css";
 
-function Navbar(){
+function Navbar() {
   return (
-    <nav id='navbar'>
+    <nav id="navbar">
+      <ul className="navbar-nav grid-container">
+        <Link to="/" className="nav-link">
+          Home{" "}
+        </Link>
 
-        <ul className="navbar-nav grid-container">
-            <Link to="/" className="nav-link">Home </Link>           
-          
-            <Link to="/portfolio" className="nav-link" >Portfolio</Link>
-          
-            <Link className="nav-link" to="/contact">Contact</Link>
-          
+        <Link to="/portfolio" className="nav-link">
+          Projects
+        </Link>
 
-        </ul>
-
+        <Link to="/contact" className="nav-link">
+          Contact
+        </Link>
+      </ul>
     </nav>
-  )
+  );
 }
 
 export default Navbar;
