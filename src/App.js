e725779a46b615b29a429a49a6640fda77974804
Camerone Stoney <Cameronestoney@yahoo.com>
2020-12-01 14:59:36 -0800

    refactor: moved the browser router to the correct location

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 import React from "react";
-import Navbar from "./components/parts/navbar/navbar";
-import { BrowserRouter, Route } from "react-router-dom";
 import Home from "./pages/home/home";
-import Portfolio from "./pages/portfolio/portfolio";
-import Contact from "./pages/contact/contact";
 
 class App extends React.Component {
-  state = {};
+  state = {
+    scrollY: 0,
+  };
 
+  handleScroll = (event) => {
+    this.setState({ scrollY: window.scrollY });
+    console.log(window.scrollY);
+  };
   render() {
     return (
-      <BrowserRouter>
-        <Navbar />
-        <Route exact path="/" component={Home} />
-        <Route path="/portfolio" component={Portfolio} />
-        <Route path="/contact" component={Contact} />
-      </BrowserRouter>
+      <div>
+        <Home />
+      </div>
     );
   }
 }
