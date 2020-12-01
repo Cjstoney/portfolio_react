d088a31466e70daf4606a5930b9eeff4f0ae9a92
Camerone Stoney <Cameronestoney@yahoo.com>
2020-12-01 15:00:34 -0800

    refactor: home page fades in on load

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,30 +1,23 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import About from "../../components/details/about/about";
 import "./home.css";
 
 const Home = () => {
-  const [loading, setLoading] = useState(true);
-
   useEffect(() => {
-    setTimeout(() => setLoading(false), 1000);
+    let el = document.querySelector(".homePage");
+    el.classList.add("fade-in");
   }, []);
 
   return (
     <>
-      {loading === false ? (
-        <div>
-          <div id="landing-section">
-            <div className="text-container">
-              <p className="static-text">Hi, I'm Camerone</p>
-            </div>
+      <div className="homePage">
+        <div id="landing-section">
+          <div className="text-container">
+            <p className="static-text">Hi, I'm Camerone</p>
           </div>
-          <About />
         </div>
-      ) : (
-        <div>
-          <p>'Hello world'</p>
-        </div>
-      )}
+        <About />
+      </div>
     </>
   );
 };
