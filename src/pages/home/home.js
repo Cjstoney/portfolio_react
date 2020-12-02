a7d2335fdfb29f7d0b551713631ab2cc81317919
Camerone Stoney <Cameronestoney@yahoo.com>
2020-12-01 16:47:17 -0800

    style: fade in

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,25 +1,22 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import About from "../../components/details/about/about";
 import "./home.css";
 
 const Home = () => {
-  const [loading, setLoading] = useState(true);
-
   useEffect(() => {
-    setTimeout(() => setLoading(false), 1000);
+    let el = document.querySelector(".homePage");
+    el.classList.add("fade-in");
   }, []);
 
   return (
     <>
-      <div>
+      <div className="homePage">
         <div id="landing-section">
           <div className="text-container">
             <p className="static-text">Hi, I'm Camerone</p>
           </div>
         </div>
-        <div>
-          <About />
-        </div>
+        <About />
       </div>
     </>
   );
