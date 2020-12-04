5bdfabaf17b82b22e19804d80d720e261f7219cb
Camerone Stoney <Cameronestoney@yahoo.com>
2020-12-04 10:35:13 -0800

    chores

--- src/components/details/about/about.js
+++ src/components/details/about/about.js
@@ -7,10 +7,10 @@ const About = () => {
     <div id="aboutSection">
       <div className="about-content">
         <p className="bio bio-text" id="cs-text">
-          <strong className="introduction">Hey, I'm Camerone.</strong> I moved
-          from the San Francisco Bay Area to Portland, Oregon in 2015. I have
-          since begun working in web development, using my art and woodworking
-          past to inform how I build websites from start to finish.{" "}
+          <strong className="introduction">Hey, I'm Camerone.</strong> <br /> I
+          moved from the San Francisco Bay Area to Portland, Oregon in 2015. I
+          have since begun working in web development, using my art and
+          woodworking past to inform how I build websites from start to finish.{" "}
         </p>
       </div>
     </div>
--- src/components/parts/navbar/navbar.js
+++ src/components/parts/navbar/navbar.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
 import { Menu, MenuOpen } from "@material-ui/icons";
 import "./navbar.css";
 
