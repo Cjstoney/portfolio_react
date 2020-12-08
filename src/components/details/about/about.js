71ff773d5800557fe9e283dfd237d7d3a8d4f9dd
Camerone Stoney <Cameronestoney@yahoo.com>
2020-12-07 19:09:48 -0800

    style: updates to about

--- src/components/details/about/about.js
+++ src/components/details/about/about.js
@@ -6,10 +6,10 @@ const About = () => {
   return (
     <div id="aboutSection">
       <div className="about-content">
+        <span className="introduction">
+          Hey, I'm <span className="name">Camerone.</span>
+        </span>{" "}
         <p className="bio bio-text" id="cs-text">
-          <strong className="introduction">
-            Hey, I'm <strong className="name">Camerone.</strong>
-          </strong>{" "}
           <br /> I moved from the San Francisco Bay Area to Portland, Oregon in
           2015. I have since begun working in web development, using my art and
           woodworking past to inform how I build websites from start to finish.{" "}
