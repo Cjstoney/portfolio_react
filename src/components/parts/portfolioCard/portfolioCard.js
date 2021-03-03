74f09ebb7b1676fffa0bcbfad68a46c5b383c5c1
Camerone Stoney <Cameronestoney@yahoo.com>
2021-03-02 17:11:18 -0800

    style: updates to portfolio section

--- src/components/parts/portfolioCard/portfolioCard.js
+++ src/components/parts/portfolioCard/portfolioCard.js
@@ -4,13 +4,18 @@ import "./portfolioCard.css";
 const PortfolioCard = (props) => {
   return (
     <li className="portfolio-card">
+      <div className="backgroundDiv"></div>
       <a
-        className="projects-image top-row"
+        className="project-link"
         target="_blank"
         rel="noopener noreferrer"
         href={props.url}
       >
         <img className="image" src={props.mockup} alt={props.name} />
+        <div className="description">
+          <h1>{props.name}</h1>
+          <p className="applicationDescription">{props.description}</p>
+        </div>
       </a>
     </li>
   );
