eea8bed5ffc1d3571ae48a8f79ea595a05dc3406
Camerone Stoney <Cameronestoney@yahoo.com>
2021-03-04 21:00:42 -0800

    style: big style updates to about section

--- src/components/sections/about/about.js
+++ src/components/sections/about/about.js
@@ -1,9 +1,20 @@
 import React from "react";
+import { useState } from "react";
 import Waves from "../../parts/waves";
 
 import "./about.css";
 
 const About = () => {
+  const [moreInfo, toggleInfo] = useState(false);
+
+  const shortBio = `I moved from the San Francisco Bay Area to Portland, Oregon in 2015. I
+  have since begun working in web development, using my art and
+  woodworking past to inform how I build websites from start to finish.`;
+
+  const extraInfo = `${shortBio} I spent three years at The San Francisco Art Institute studying Fine Art with an emphasis in painting and photography.
+  After I graduated, I moved to Portland with my wife and our friend where I got a job as a production woodworker, helping to build custom greenhouses. After three years I decided to 
+  learn web development and transition careers. `;
+
   return (
     <div className="about-content">
       <h1 className="introduction">
@@ -12,11 +23,10 @@ const About = () => {
       </h1>
 
       <Waves />
-      <p className="bio ">
-        <br /> I moved from the San Francisco Bay Area to Portland, Oregon in
-        2015. I have since begun working in web development, using my art and
-        woodworking past to inform how I build websites from start to finish.{" "}
-      </p>
+      <p className="bio ">{`${!!moreInfo ? extraInfo : shortBio}`}</p>
+      <button className="learnMoreBtn" onClick={() => toggleInfo(!moreInfo)}>
+        {`Want to know ${!!moreInfo ? "less" : "more"}?`}
+      </button>
     </div>
   );
 };
