85bd790563fc216ae78b328ea3bc0aabcc20c937
Camerone Stoney <Cameronestoney@yahoo.com>
2021-03-05 10:56:38 -0800

    chore: long info section added

--- src/components/sections/about/about.js
+++ src/components/sections/about/about.js
@@ -1,17 +1,22 @@
 import React from "react";
 import { useState } from "react";
 import Waves from "../../parts/waves";
-
 import "./about.css";
 
 const About = () => {
   const [moreInfo, toggleInfo] = useState(false);
 
+  const addClass = () => {
+    const el = document.getElementById("bioId");
+    el.setAttribute("class", "longBio");
+    toggleInfo(!moreInfo);
+  };
   const shortBio = `I moved from the San Francisco Bay Area to Portland, Oregon in 2015. I
   have since begun working in web development, using my art and
   woodworking past to inform how I build websites from start to finish.`;
 
-  const extraInfo = `${shortBio} I spent three years at The San Francisco Art Institute studying Fine Art with an emphasis in painting and photography.
+  const extraInfo = `${shortBio} 
+  After growing up in the Bay Area, I spent three years at The San Francisco Art Institute studying Fine Art with an emphasis in painting and photography.
   After I graduated, I moved to Portland with my wife and our friend where I got a job as a production woodworker, helping to build custom greenhouses. After three years I decided to 
   learn web development and transition careers. `;
 
@@ -21,10 +26,11 @@ const About = () => {
         HI, I'M <br />
         CAMERONE
       </h1>
-
       <Waves />
-      <p className="bio ">{`${!!moreInfo ? extraInfo : shortBio}`}</p>
-      <button className="learnMoreBtn" onClick={() => toggleInfo(!moreInfo)}>
+      <p id="bioId" className="bio ">{`${
+        !!moreInfo ? extraInfo : shortBio
+      }`}</p>
+      <button className="learnMoreBtn" onClick={() => addClass()}>
         {`Want to know ${!!moreInfo ? "less" : "more"}?`}
       </button>
     </div>
