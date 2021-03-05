645bf58f9d5b844027ccce08acd49c2118ad8872
Camerone Stoney <Cameronestoney@yahoo.com>
2021-03-04 17:08:16 -0800

    style: start of about section styles

--- src/components/sections/about/about.js
+++ src/components/sections/about/about.js
@@ -1,20 +1,22 @@
 import React from "react";
+import Waves from "../../parts/waves";
 
 import "./about.css";
 
 const About = () => {
   return (
-    <div id="aboutSection">
-      <div className="about-content">
-        <span className="introduction">
-          Hey, I'm <span className="name">Camerone.</span>
-        </span>{" "}
-        <p className="bio bio-text" id="cs-text">
-          <br /> I moved from the San Francisco Bay Area to Portland, Oregon in
-          2015. I have since begun working in web development, using my art and
-          woodworking past to inform how I build websites from start to finish.{" "}
-        </p>
-      </div>
+    <div className="about-content">
+      <h1 className="introduction">
+        HI, I'M <br />
+        CAMERONE
+      </h1>
+
+      <Waves />
+      <p className="bio ">
+        <br /> I moved from the San Francisco Bay Area to Portland, Oregon in
+        2015. I have since begun working in web development, using my art and
+        woodworking past to inform how I build websites from start to finish.{" "}
+      </p>
     </div>
   );
 };
