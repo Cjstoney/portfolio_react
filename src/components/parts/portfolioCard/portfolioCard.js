38727c491eb2ebc413f42d546b36825c9e7476eb
Camerone Stoney <Cameronestoney@yahoo.com>
2021-03-05 10:56:22 -0800

    refactor: updated the component to no longer need a wrapper

--- src/components/parts/portfolioCard/portfolioCard.js
+++ src/components/parts/portfolioCard/portfolioCard.js
@@ -1,37 +1,40 @@
 import React from "react";
 import "./portfolioCard.css";
+import cards from "../../../cards.json";
 
-const PortfolioCard = (props) => {
-  return (
-    <li className="portfolio-card">
-      <div className="backgroundDiv"></div>
-      <a
-        className="project-link"
-        target="_blank"
-        rel="noopener noreferrer"
-        href={props.url}
-      >
-        <img className="image" src={props.mockup} alt={props.name} />
-        <div className="description">
-          <h1>{props.name}</h1>
-          <p className="applicationDescription">{props.description}</p>
-          <ul className="technologies">
-            {props.technologies.map((tech) => (
-              <li
-                className={`${props.name}-${tech.technology} technologyListItem`}
-              >
-                <img
-                  className="techlogo"
-                  src={tech.techImage}
-                  alt={`${tech.technology} icon`}
-                />
-              </li>
-            ))}
-          </ul>
-        </div>
-      </a>
-    </li>
-  );
+const PortfolioCard = () => {
+  return cards.reverse().map((card) => {
+    return (
+      <li className="portfolio-card">
+        <div className="backgroundDiv"></div>
+        <a
+          className="project-link"
+          target="_blank"
+          rel="noopener noreferrer"
+          href={card.url}
+        >
+          <img className="image" src={card.mockup} alt={card.name} />
+          <div className="description">
+            <h1>{card.name}</h1>
+            <p className="applicationDescription">{card.description}</p>
+            <ul className="technologies">
+              {card.technologies.map((tech) => (
+                <li
+                  className={`${card.name}-${tech.technology} technologyListItem`}
+                >
+                  <img
+                    className="techlogo"
+                    src={tech.techImage}
+                    alt={`${tech.technology} icon`}
+                  />
+                </li>
+              ))}
+            </ul>
+          </div>
+        </a>
+      </li>
+    );
+  });
 };
 
 export default PortfolioCard;
