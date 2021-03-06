b281e7b1ba07c48d0676667f94b8ee84e3226305
Camerone Stoney <Cameronestoney@yahoo.com>
2021-03-05 19:18:38 -0800

    style: added some space

--- src/components/parts/portfolioCard/portfolioCard.js
+++ src/components/parts/portfolioCard/portfolioCard.js
@@ -3,38 +3,42 @@ import "./portfolioCard.css";
 import cards from "../../../cards.json";
 
 const PortfolioCard = () => {
-  return cards.reverse().map((card) => {
-    return (
-      <li className="portfolio-card">
-        <div className="backgroundDiv"></div>
-        <a
-          className="project-link"
-          target="_blank"
-          rel="noopener noreferrer"
-          href={card.url}
-        >
-          <img className="image" src={card.mockup} alt={card.name} />
-          <div className="description">
-            <h1>{card.name}</h1>
-            <p className="applicationDescription">{card.description}</p>
-            <ul className="technologies">
-              {card.technologies.map((tech) => (
-                <li
-                  className={`${card.name}-${tech.technology} technologyListItem`}
-                >
-                  <img
-                    className="techlogo"
-                    src={tech.techImage}
-                    alt={`${tech.technology} icon`}
-                  />
-                </li>
-              ))}
-            </ul>
-          </div>
-        </a>
-      </li>
-    );
-  });
+  return (
+    <ul className="listContainer">
+      {cards.reverse().map((card) => {
+        return (
+          <li className="portfolio-card">
+            <div className="backgroundDiv"></div>
+            <a
+              className="project-link"
+              target="_blank"
+              rel="noopener noreferrer"
+              href={card.url}
+            >
+              <img className="image" src={card.mockup} alt={card.name} />
+              <div className="description">
+                <h1>{card.name}</h1>
+                <p className="applicationDescription">{card.description}</p>
+                <ul className="technologies">
+                  {card.technologies.map((tech) => (
+                    <li
+                      className={`${card.name}-${tech.technology} technologyListItem`}
+                    >
+                      <img
+                        className="techlogo"
+                        src={tech.techImage}
+                        alt={`${tech.technology} icon`}
+                      />
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </a>
+          </li>
+        );
+      })}
+    </ul>
+  );
 };
 
 export default PortfolioCard;
