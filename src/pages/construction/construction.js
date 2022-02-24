aad605d03fbb5f551f2202fe559c91f4e6f1bdac
Camerone Stoney <Cameronestoney@yahoo.com>
2022-02-24 13:04:54 -0800

    feat:construction component

---
+++ src/pages/construction/construction.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./construction.css";
+
+export default function Construction() {
+  return (
+    <div className="constructionPage">
+      <div className="textContainer">
+        <h1 className="constructionText">portfolio in progress</h1>
+        <h2 className="developerName">camerone Stoney</h2>
+        <h3 className="textExplanation">
+          developer / implementation engineer / woodworker / avid learner
+        </h3>
+      </div>
+    </div>
+  );
+}
