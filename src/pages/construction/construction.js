dce0fe7ac9ca6beaca22158b7e6ff4fbf145f09e
Camerone Stoney <Cameronestoney@yahoo.com>
2022-02-24 14:14:49 -0800

    refactor: updated to add email link

--- src/pages/construction/construction.js
+++ src/pages/construction/construction.js
@@ -1,15 +1,29 @@
 import React from "react";
 import "./construction.css";
+import "@fontsource/roboto-mono";
 
 export default function Construction() {
   return (
     <div className="constructionPage">
       <div className="textContainer">
-        <h1 className="constructionText">portfolio in progress</h1>
-        <h2 className="developerName">camerone Stoney</h2>
+        <h1 className="constructionText" id="constructionTitle">
+          portfolio in progress
+        </h1>
+        <h2 className="developerName" id="nameHeader">
+          camerone Stoney
+        </h2>
         <h3 className="textExplanation">
           developer / implementation engineer / woodworker / avid learner
         </h3>
+        <a
+          href="mailto:camjstoney@gmail.com?subject=Found your portfolio!"
+          className="emailLink"
+          id="directEmail"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          get in touch
+        </a>
       </div>
     </div>
   );
