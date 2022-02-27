4ae18af5aa7cc14590e0c06978c4d220ae160cd9
Camerone Stoney <Cameronestoney@yahoo.com>
2022-02-26 21:10:05 -0800

    refactor: rip out fade in

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -4,15 +4,10 @@ import "./home.css";
 import PortfolioCard from "../../components/portfolioCard/portfolioCard";
 
 const Home = () => {
-  useEffect(() => {
-    let el = document.querySelector(".homePage");
-    el.classList.add("fade-in");
-  }, []);
-
   return (
     <div className="homePage">
       <About className="aboutSection" />
-      <PortfolioCard />
+      {/* <PortfolioCard /> */}
     </div>
   );
 };
