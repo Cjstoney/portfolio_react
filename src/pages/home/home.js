9ec7222438846717904c2d33b4145dec9f9c0341
Camerone Stoney <Cameronestoney@yahoo.com>
2022-03-02 16:12:41 -0800

    chore: removing the portfolio card from render

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,13 +1,11 @@
-import React, { useEffect } from "react";
-import About from "../../components/about/about";
+import React from "react";
+import Introduction from "../../components/introduction/introduction";
 import "./home.css";
-import PortfolioCard from "../../components/portfolioCard/portfolioCard";
 
 const Home = () => {
   return (
     <div className="homePage">
-      <About className="aboutSection" />
-      {/* <PortfolioCard /> */}
+      <Introduction className="aboutSection" />
     </div>
   );
 };
