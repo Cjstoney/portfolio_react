6435d1d901ec3a05ffbad66ac31134704c19a75c
Camerone Stoney <Cameronestoney@yahoo.com>
2022-03-02 20:51:36 -0800

    chore: updated components to module

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import AboutMe from "./about/aboutMe";
+import Introduction from "./introduction/introduction";
+import Experience from "./experience/experience";
+
+export const AboutMeComponent = AboutMe;
+export const IntroductionComponent = Introduction;
+export const ExperienceComponent = Experience;
