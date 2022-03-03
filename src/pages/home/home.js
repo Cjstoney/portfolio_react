2d5f213d5a933c7b04ed39f092ac5ac269d5e544
Camerone Stoney <Cameronestoney@yahoo.com>
2022-03-02 20:12:56 -0800

    chore: adding about me to home

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Introduction from "../../components/introduction/introduction";
+import AboutMe from "../../components/about/aboutMe";
 import "./home.css";
 
 const Home = () => {
   return (
     <div className="homePage">
-      <Introduction className="aboutSection" />
+      <Introduction className="about-content" />
+      <AboutMe className="about-me-container" />
     </div>
   );
 };
