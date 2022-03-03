c63c35b7c7b9a68aae3cf48fc641270fbe62060c
Camerone Stoney <Cameronestoney@yahoo.com>
2022-03-02 17:58:22 -0800

    refactor: updating button

--- src/components/introduction/introduction.js
+++ src/components/introduction/introduction.js
@@ -19,6 +19,16 @@ const Introduction = () => {
           <span className="text-accent-color"> Implementation Engineer</span>.
         </p>
       </div>
+      <a
+        href="mailto:camjstoney@gmail.com?subject=Found your portfolio!"
+        className="emailLink"
+        id="directEmail"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Let's Connect
+      </a>
+
       <picture className="picture">
         <img
           src="/images/cameroneStoney.png"
