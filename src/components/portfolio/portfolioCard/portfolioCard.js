c6569d8d78be697bc2699505de40d285e916380c
Camerone Stoney <Cameronestoney@yahoo.com>
2022-03-03 20:17:42 -0800

    feat:portfolio code

--- src/components/portfolio/portfolioCard/portfolioCard.js
+++ src/components/portfolio/portfolioCard/portfolioCard.js
@@ -1,9 +1,16 @@
 import React from "react";
+import "./portfolioCard.css";
+import { GithubSVGOutlineComponent, FolderIconComponent } from "../../index";
 
 function PortfolioCard({ className, index, card }) {
   return (
     <div className={className} key={index}>
-      testing {card.id}
+      <FolderIconComponent className="folder-icon" />
+      <GithubSVGOutlineComponent className="gh-outline" />
+      <div className="proj-title">
+        {card.projName}
+        <div className="proj-description">{card.description}</div>
+      </div>
     </div>
   );
 }
