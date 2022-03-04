9ad9d3d47b79f7eea13201206248c313c9d23365
Camerone Stoney <Cameronestoney@yahoo.com>
2022-03-04 07:47:35 -0800

    feat: portfolio card creation

--- src/components/portfolio/portfolioCard/portfolioCard.js
+++ src/components/portfolio/portfolioCard/portfolioCard.js
@@ -2,14 +2,30 @@ import React from "react";
 import "./portfolioCard.css";
 import { GithubSVGOutlineComponent, FolderIconComponent } from "../../index";
 
-function PortfolioCard({ className, index, card }) {
+function PortfolioCard({ className, card }) {
   return (
-    <div className={className} key={index}>
+    <div className={className}>
       <FolderIconComponent className="folder-icon" />
-      <GithubSVGOutlineComponent className="gh-outline" />
+      <a
+        className="gh-outline"
+        href={card.ghURL}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <GithubSVGOutlineComponent />
+      </a>
       <div className="proj-title">
         {card.projName}
         <div className="proj-description">{card.description}</div>
+      </div>
+      <div className="proj-tech">
+        {card.technologies.map((tech, index) => {
+          return (
+            <div className="individual-tech" key={`tech-${index}`}>
+              {tech.technology}
+            </div>
+          );
+        })}
       </div>
     </div>
   );
