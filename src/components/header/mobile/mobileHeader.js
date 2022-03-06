3628ff3118423129659746cf587a1abbc82f7669
Camerone Stoney <Cameronestoney@yahoo.com>
2022-03-06 14:26:24 -0800

    chore: mobile menu removed

--- src/components/header/mobile/mobileHeader.js
+++ src/components/header/mobile/mobileHeader.js
@@ -6,9 +6,6 @@ function MobileHeader({ className }) {
   return (
     <header className={className}>
       <LogoComponent className="mobile-logo" />
-      <button className="mobile-menu-btn">
-        <MenuTripleDotComponent className="trip-dot-menu" />
-      </button>
     </header>
   );
 }
